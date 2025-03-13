2ca1cbb7283945fff8f0876457fc7ba9f58c9bab
aravindm52 <aravindmurugesan2005@gmail.com>
2025-03-13 10:44:07 +0530

    this is my conditional operator

---
+++ operator/operator.java
@@ -0,0 +1,10 @@
+public class operator{
+    Arithmetic operator 
+    public static void main(string args[]){
+        system.out.println(10+2);
+        system.out.println(10-2);
+        system.out.println(10*2);
+        system.out.println(10/2);
+        system.out.println(10%2);
+    }
+}
